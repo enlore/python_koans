fe6bff56ceaa71b959b4c927651157cd3d550057
enlore <n.e.lorenson@gmail.com>
2013-07-15 15:05:24 -0500

    Doin the things that a triangle can python contemplate_koans.py

--- python2/koans/triangle.py
+++ python2/koans/triangle.py
@@ -19,8 +19,31 @@
 #
 def triangle(a, b, c):
     # DELETE 'PASS' AND WRITE THIS CODE
-    pass
+    # sane sides check:
+    # the sum of any two sides must be greater than the third
+    def checker(op, op1, side):
+        if op + op1 <= side:
+            raise TriangleError
 
+    for op, op1, side in [
+            (a, b, c), (a, c, b), 
+            (b, c, a), (b, a, c), 
+            (c, a, b), (c, b, a)]:
+        checker(op, op1, side)
+
+    if a < 1 or b < 1 or c < 1:
+        raise TriangleError
+
+    if a == b == c and a != 0:
+        return 'equilateral'
+
+    if a == b or a == c or b == c:
+        return 'isosceles'
+
+    if a != b and a != c and b != c:
+        return 'scalene'
+
+    raise TriangleError
 
 # Error class used in part 2.  No need to change this code.
 class TriangleError(StandardError):
